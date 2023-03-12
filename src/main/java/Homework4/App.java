7e91f4c3dd3475b1c449ea9a6e4499a14b83ff97
roger <selas.lucian@gmail.com>
2023-03-12 22:38:34 +0200

    Ver1

--- src/main/java/Homework4/App.java
+++ src/main/java/Homework4/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args ) {
 
-    Ford car1 = new Ford( "Focus", "QWERTY123456", 6, "diesel", 45, 45, 5.8f, 15);
+    Vehicle car1 = new Ford( "Focus", "QWERTY123456", 6, "diesel", 45, 45, 5.8f, 15);
 
     car1.Start();
 
