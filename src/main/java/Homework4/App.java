8d145bdb70ebc97cbadf432bdde100046f8c80a7
roger <seles.lucian@gmail.com>
2023-03-15 12:05:25 +0200

    Modify1

--- src/main/java/Homework4/App.java
+++ src/main/java/Homework4/App.java
@@ -10,6 +10,7 @@ public class App
 
     Vehicle car1 = new Ford( "Focus", "QWERTY123456", 6, "diesel", 45, 45, 5.8f, 15);
 
+
     car1.Start();
 
     }
--- src/main/java/Homework4/Ford.java
+++ src/main/java/Homework4/Ford.java
@@ -4,14 +4,14 @@ public class Ford extends Car {
     protected String carModel;
     public float availableFuel;
     public int tireSize;
-    public String chassisNumber;
+    private static String CHASSIS_NUMBER;
 
-    public Ford (String carModel, String chassisNumber, int gears, String fuelType, int fuelTankSize, float availableFuel, float consPer100Km, int tireSize) {
+    public Ford (String carModel, String CHASSIS_NUMBER, int gears, String fuelType, int fuelTankSize, float availableFuel, float consPer100Km, int tireSize) {
 
         super(fuelType, fuelTankSize, gears, consPer100Km);
 
         this.carModel = carModel;
-        this.chassisNumber = chassisNumber;
+        this.CHASSIS_NUMBER = CHASSIS_NUMBER;
         this.tireSize = tireSize;
         this.availableFuel = availableFuel;
 
